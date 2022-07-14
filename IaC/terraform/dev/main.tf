8a8b28129696d1a047aee2ab38848d686620fa45
Ross Flood <rflood@eaglebrookadvisors.com>
2022-07-14 12:56:49 -0500

    added tag

--- IaC/terraform/dev/main.tf
+++ IaC/terraform/dev/main.tf
@@ -1,5 +1,15 @@
 
 
+locals {
+  required_tags = {
+    project     = var.project_name,
+    environment = dev
+    location    = var.location
+  }
+  tags        = merge(var.resource_tags, local.required_tags)
+  name_prefix = "${var.environment}-${var.project_name}-"
+}
+
 #Data section
 data "azurerm_resource_group" "ResGroup" {
   name = var.resource_group
@@ -14,10 +24,12 @@ resource "azurerm_storage_account" "StorAccount1" {
   account_kind             = "StorageV2"
   account_tier             = "Standard"
   account_replication_type = var.replicationType
+  tags                     = local.tags
 }
 
 resource "azurerm_storage_container" "Container1" {
   name                  = "images"
   storage_account_name  = azurerm_storage_account.StorAccount1.name
   container_access_type = "private"
+  tags                  = local.tags
 }
