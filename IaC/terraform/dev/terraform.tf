1dab92ef3ddaf4687b8b574cd3108f489bda02e8
Ross Flood <rflood@eaglebrookadvisors.com>
2022-07-14 09:33:31 -0500

    added envs

--- IaC/terraform/dev/terraform.tf
+++ IaC/terraform/dev/terraform.tf
@@ -19,6 +19,5 @@ terraform {
 ##################################################################################
 
 provider "azurerm" {
-  version = "=2.11"
   features {} #This is required for v2 of the provider even if empty or plan will fail
 }
