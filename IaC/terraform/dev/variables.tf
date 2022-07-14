85125acb26ab006451511800614c06126327ff5c
Ross Flood <rflood@eaglebrookadvisors.com>
2022-07-14 12:56:43 -0500

    add pr

--- IaC/terraform/dev/variables.tf
+++ IaC/terraform/dev/variables.tf
@@ -3,6 +3,17 @@
 # Resource group
 #############################################################################
 
+variable "project_name" {
+  description = "Name of the project."
+  type        = string
+  default     = "my-project"
+}
+
+variable "environment" {
+  description = "Name of the environment."
+  type        = string
+  default     = "dev"
+}
 
 variable "resource_group" {
   description = "The type of replication"
@@ -10,12 +21,25 @@ variable "resource_group" {
   default     = "dev-eba-dflt-eastus-linux"
 }
 
+variable "location" {
+  type    = string
+  default = "eastus"
+}
+
+variable "resource_tags" {
+  description = "Tags to set for all resources"
+  type        = map(string)
+  default = {
+    project     = "my-project",
+    environment = "dev"
+  }
+}
 #############################################################################
 # STORAGE ACCOUNT
 #############################################################################
 
 variable "storage_account_name" {
-  description = "The type of replication"
+  description = "storage account"
   type        = string
   default     = "ebadfltstorage"
 }
@@ -31,25 +55,20 @@ variable "replicationType" {
 # APP SERVICE PLAN
 #############################################################################
 
-variable "location" {
-  type    = string
-  default = "east us"
-}
-
 variable "asp_tier" {
-    type = string
-    description = "Tier for App Service Plan (Standard, PremiumV2)"
-    default = "Standard"
+  type        = string
+  description = "Tier for App Service Plan (Standard, PremiumV2)"
+  default     = "Standard"
 }
 
 variable "asp_size" {
-    type = string
-    description = "Size for App Service Plan (S2, P1v2)"
-    default = "S1"
+  type        = string
+  description = "Size for App Service Plan (S2, P1v2)"
+  default     = "S1"
 }
 
 variable "capacity" {
-  type = string
+  type        = string
   description = "Number of instances for App Service Plan"
-  default = "1"
+  default     = "1"
 }
